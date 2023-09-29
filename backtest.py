705f2ac37afc2d1e4289520292968a8aab07974e
bsiyoung <siyoung4528@gmail.com>
2023-09-30 04:44:47 +0900

    Re-Write DataManager

--- backtest.py
+++ backtest.py
@@ -1,8 +1,106 @@
 import os
-from datamgr.datamgr import DataManager
 
+from datetime import timedelta
+
+from datamgr.datamgr import DataManager
+from datamgr.interval_conf import interval_deltas
 
 class BackTest:
-    def __init__(self, root_dir='.'):
+    def __init__(self, tickers, intervals=None, force_update=False, root_dir='.'):
         self.root = root_dir
+
+        if intervals is None:
+            intervals = ['1d', '1w', '1M']
+
         self.dm = DataManager(root_dir=os.path.join(self.root, 'datamgr'))
+        self.dm.update(tickers, force_update=force_update, verbose=True)
+        print()
+        self.dm.update_acc(tickers, intervals=intervals, force_update=force_update, verbose=True)
+
+        self.data = dict()
+        for ticker in tickers:
+            data_buf = self.dm.get(ticker, acc=True)
+            self.data[ticker] = data_buf
+        self.__match_data()
+
+    def __match_data(self):
+        datas = []
+        for ticker in list(self.data.keys()):
+            for interval in list(self.data[ticker].keys()):
+                datas.append(self.data[ticker][interval])
+        self.dm.match(datas)
+
+        idx = 0
+        for ticker in list(self.data.keys()):
+            for interval in list(self.data[ticker].keys()):
+                self.data[ticker][interval] = datas[idx]
+                idx += 1
+
+    def get_tickers(self):
+        return list(self.data.keys())
+
+    def get_data_datetime_range(self):
+        if len(self.data) == 0:
+            return None, None
+
+        sample_ticker = self.get_tickers()[0]
+        sample_interval = list(self.data[sample_ticker].keys())[0]
+        sample_data = self.data[sample_ticker][sample_interval]
+
+        return sample_data.index[0], sample_data.index[-1]
+
+    def new_sim(self, st_tm=None, en_tm=None, sz_step=timedelta(minutes=1)):
+        datetime_rng = self.get_data_datetime_range()
+
+        if st_tm is None:
+            st_tm = datetime_rng[0]
+
+        if en_tm is None:
+            en_tm = datetime_rng[1]
+
+        sim = self.Simulator(self, st_tm, en_tm, sz_step)
+        return sim
+
+    class Simulator:
+        def __init__(self, parent, st_tm, en_tm, sz_step):
+            self.parent = parent
+            self.data = parent.data
+            self.curr_tm = st_tm
+            self.en_tm = en_tm
+            self.sz_step = sz_step
+
+        def get_tickers(self):
+            return list(self.parent.data.keys())
+
+        def get_data_datetime_range(self):
+            return self.parent.get_data_datetime_range()
+
+        def step(self, sz_step=None):
+            if sz_step is not None:
+                sz_step = self.sz_step
+
+            if self.curr_tm + sz_step > self.en_tm:
+                return False
+
+            self.curr_tm += sz_step
+            return True
+
+        def get_status(self):
+            pass
+
+
+def test():
+    tickers = ['^IXIC', 'QQQ', 'QLD', 'TQQQ', 'PSQ', 'QID']
+
+    bt = BackTest(tickers, force_update=False)
+    print()
+    datetime_rng = bt.get_data_datetime_range()
+    print('Datetime Range : {}, {}'.format(datetime_rng[0], datetime_rng[1]))
+    print('Tickers :', bt.get_tickers())
+
+    sim1 = bt.new_sim(st_tm=datetime_rng[0], en_tm=datetime_rng[1],
+                      sz_step=interval_deltas['1m'])
+
+
+if __name__ == '__main__':
+    test()
--- datamgr/datamgr.py
+++ datamgr/datamgr.py
@@ -1,220 +1,161 @@
 import os
 from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
-import time
 import pickle
+import time
 
 import numpy as np
 import pandas as pd
 import yfinance as yf
 
 
-class EmptyData(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return self.msg
-
-
 class DataManager:
-    def __init__(self, root_dir='.'):
-        self.root = os.path.abspath(root_dir)
-        self.cache_dir = os.path.join(self.root, 'cache')
-        if not os.path.exists(self.cache_dir):
-            os.mkdir(self.cache_dir)
+    def __init__(self, cache_path):
+        self.cache_path = os.path.abspath(cache_path)
 
-    def get(self, ticker, acc=False):
-        cache_file = '{}{}.cache'.format(ticker, '_acc' if acc else '')
-        cache_path = os.path.join(self.cache_dir, cache_file)
-
-        if not os.path.isfile(cache_path):
-            return None
-
-        rf = open(cache_path, 'rb')
-        data = pickle.load(rf)
-
-        return data
-
-    def update(self, tickers, progress=True, force_update=False, update_dt=86400):
+    def update(self, tickers, force_update=False, update_freq=80000, verbose=True):
         if type(tickers) is str:
             tickers = [tickers]
 
-        end_date = datetime.now() + timedelta(days=1)
-        for idx, ticker in enumerate(tickers):
-            cache_file = '{}.cache'.format(ticker)
-            cache_path = os.path.join(self.cache_dir, cache_file)
+        if not os.path.exists(self.cache_path):
+            os.makedirs(self.cache_path)
 
-            if os.path.isfile(cache_path) and force_update is False:
-                m_time = os.path.getmtime(cache_path)
+        start_date = '1900-01-01'
+        end_date = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
 
-                # Don't update if data is recently updated
-                if (time.time() - m_time) <= update_dt:
-                    if progress is True:
-                        print('({}/{}) [{}] Recently updated'.format(idx + 1, len(tickers), ticker))
+        for i, ticker in enumerate(tickers):
+            file_name = '{}.cache'.format(ticker)
+            file_path = os.path.join(self.cache_path, file_name)
+
+            if os.path.isfile(file_path) and force_update is False:
+                # Check file modification time
+                if time.time() - os.path.getmtime(file_path) < update_freq:
+                    if verbose:
+                        print('({}/{}) [{}] Pass'.format(i + 1, len(tickers), ticker))
                     continue
 
-            data = yf.download(ticker,
-                               '1950-01-01', end_date.strftime("%Y-%m-%d"),
-                               progress=False)
+            data = yf.download(ticker, start_date, end_date, progress=False)
             if len(data) == 0:
-                if progress is True:
-                    print('({}/{}) [{}] Received empty data'.format(idx + 1, len(tickers), ticker))
+                if verbose:
+                    print('({}/{}) [{}] Empty data'.format(i + 1, len(tickers), ticker))
                 continue
 
-            # Insert adjusted data columns
-            adj_open = data['Open'].values / data['Close'].values * data['Adj Close']
-            data.insert(4, 'Adj Open', adj_open)
+            adj_mult = data['Adj Close'] / data['Close']
+            data.insert(4, 'Adj Open', data['Open'] * adj_mult)
+            data.insert(5, 'Adj High', data['High'] * adj_mult)
+            data.insert(6, 'Adj Low', data['Low'] * adj_mult)
 
-            adj_high = data['High'].values / data['Close'].values * data['Adj Close']
-            data.insert(5, 'Adj High', adj_high)
+            wf = open(file_path, 'wb')
+            pickle.dump(data, wf)
+            wf.close()
 
-            adj_low = data['Low'].values / data['Close'].values * data['Adj Close']
-            data.insert(6, 'Adj Low', adj_low)
+            if verbose:
+                print('({}/{}) [{}] Updated'.format(i + 1, len(tickers), ticker))
 
-            # Save data
-            if not os.path.exists(self.cache_dir):
-                os.mkdir(self.cache_dir)
+    def acc_update(self, tickers, sz_acc_list=None, force_update=False, update_freq=80000, verbose=False):
+        if type(tickers) is str:
+            tickers = [tickers]
 
-            fw = open(cache_path, 'wb')
-            pickle.dump(data, fw)
+        if sz_acc_list is None:
+            sz_acc_list = [1, 5, 30]
 
-            if progress is True:
-                print('({}/{}) [{}] Data updated'.format(idx + 1, len(tickers), ticker))
+        datas = self.get(tickers)
 
-    def delete(self, ticker):
-        cache_file = '{}.cache'.format(ticker)
-        cache_path = os.path.join(self.cache_dir, cache_file)
+        for i, ticker in enumerate(list(datas.keys())):
+            file_name = '{}_acc.cache'.format(ticker)
+            file_path = os.path.join(self.cache_path, file_name)
 
-        if os.path.isfile(cache_path):
-            os.remove(cache_path)
+            if os.path.isfile(file_path) and force_update is False:
+                if time.time() - os.path.getmtime(file_path) < update_freq:
+                    if verbose:
+                        print('({}/{}) [{}](Acc) Pass'.format(i + 1, len(tickers), ticker))
 
-    @staticmethod
-    def match(dfs):
-        # Get start and end datetime
-        st_tm = None
-        en_tm = None
-
-        for df in dfs:
-            if st_tm is None:
-                st_tm = df.index[0]
-            elif st_tm < df.index[0]:
-                st_tm = df.index[0]
-
-            if en_tm is None:
-                en_tm = df.index[-1]
-            elif en_tm > df.index[-1]:
-                en_tm = df.index[0]
-
-        # Cut dataframes with st_tm and en_tm
-        for i, df in enumerate(dfs):
-            st_idx = 0
-            while df.index[st_idx] < st_tm and st_idx < len(df) - 1:
-                st_idx += 1
-
-            en_idx = st_idx
-            while df.index[en_idx] < en_tm and en_idx < len(df) - 1:
-                en_idx += 1
-            en_idx += 1
-
-            dfs[i] = df[st_idx:en_idx]
-
-        # Drop rows which are not intersection of every dataframes
-        idx_int = set(dfs[0].index)
-        for df in dfs[1:]:
-            idx_int = idx_int.intersection(set(df.index))
-
-        for i in range(len(dfs)):
-            drop_idx = list(set(dfs[i].index) - idx_int)
-            for idx in drop_idx:
-                dfs[i] = dfs[i].drop(idx)
-
-    @staticmethod
-    def accumulate(df, interval, verbose=False):
-        st_tm = df.index[0] + interval
-        st_idx = 0
-        while df.index[st_idx] < st_tm:
-            st_idx += 1
-        st_idx -= 1
-
-        cols = df.columns.values.tolist()
-        cols.append('Date')
-        res = pd.DataFrame(columns=cols)
-        res.set_index('Date', inplace=True)
-
-        total_cnt = len(df) - st_idx
-        for i in range(st_idx, len(df)):
-            curr_idx = df.index[i]
-            chunk = df[(curr_idx - interval < df.index) & (df.index <= curr_idx)]
-            new_row = {
-                'Open': chunk.iloc[0]['Open'],
-                'Adj Open': chunk.iloc[0]['Adj Open'],
-                'High': max(chunk['High']),
-                'Adj High': max(chunk['Adj High']),
-                'Low': min(chunk['Low']),
-                'Adj Low': min(chunk['Adj Low']),
-                'Close': chunk.iloc[-1]['Close'],
-                'Adj Close': chunk.iloc[-1]['Adj Close'],
-                'Volume': np.sum(chunk['Volume'])
-            }
-
-            res.loc[curr_idx] = new_row
-
-            if (i % 100 == 0 or i == len(df) - 1) and verbose is True:
-                curr_cnt = i - st_idx + 1
-                progress = round(curr_cnt / total_cnt * 100, 2)
-                print('\r{}%     '.format(progress), end='')
-
-        print()
-        return res
-
-    def update_acc(self, tickers, intervals=None, verbose=False, force_update=False, update_dt=86400):
-        if intervals is None:
-            intervals = [
-                ['1d', relativedelta(days=1)],
-                ['1w', relativedelta(weeks=1)],
-                ['1M', relativedelta(months=1)]
-            ]
-
-        for idx, ticker in enumerate(tickers):
-            cache_file = '{}_acc.cache'.format(ticker)
-            cache_path = os.path.join(self.cache_dir, cache_file)
-
-            if os.path.isfile(cache_path) and force_update is False:
-                m_time = os.path.getmtime(cache_path)
-
-                # Don't update if data is recently updated
-                if (time.time() - m_time) <= update_dt:
-                    if verbose is True:
-                        print('({}/{}) [{}](Acc) Recently updated'.format(idx + 1, len(tickers), ticker))
                     continue
 
-            data = self.get(ticker, acc=False)
+            data = datas[ticker]
+            column_names = data.columns.values.tolist()
+            column_names.append('Date')
+
             res = dict()
-            for s, delta in intervals:
-                if verbose is True:
-                    print('({}/{}) [{}](Acc) {}'.format(idx + 1, len(tickers), ticker, s))
-                acc_data = self.accumulate(data, delta, verbose=verbose)
-                res[s] = acc_data
+            for sz_acc in sz_acc_list:
+                acc_df = pd.DataFrame(columns=column_names)
+                acc_df.set_index('Date', inplace=True)
 
-            # Save data
-            if not os.path.exists(self.cache_dir):
-                os.mkdir(self.cache_dir)
+                if verbose:
+                    print('({}/{}) [{}](Acc) sz_acc={}'.format(i + 1, len(tickers), ticker, sz_acc))
 
-            fw = open(cache_path, 'wb')
-            pickle.dump(data, fw)
+                for idx in range(sz_acc - 1, len(data)):
+                    chunk = data[idx-sz_acc+1:idx+1]
+                    row_data = {
+                        'Open': chunk['Open'].values[0],
+                        'High': max(chunk['High'].values),
+                        'Low': min(chunk['Low'].values),
+                        'Close': chunk['Close'].values[-1],
+                        'Adj Open': chunk['Adj Open'].values[0],
+                        'Adj High': max(chunk['Adj High'].values),
+                        'Adj Low': min(chunk['Adj Low'].values),
+                        'Adj Close': chunk['Adj Close'].values[-1],
+                        'Volume': np.sum(chunk['Volume'].values)
+                    }
 
-            if verbose is True:
-                print('({}/{}) [{}](Acc) Data updated'.format(idx + 1, len(tickers), ticker))
+                    curr_date = chunk.index.values[-1]
+                    acc_df.loc[curr_date] = row_data
+
+                    if idx % 100 == 0 or idx == len(data) - 1:
+                        n_total_data = len(data) - sz_acc + 1
+                        curr_data = idx - sz_acc + 2
+                        progress = round(curr_data / n_total_data * 100, 2)
+                        print('\r{}%'.format(progress), end='')
+
+                if verbose:
+                    print()
+
+                res[sz_acc] = acc_df
+
+            wf = open(file_path, 'wb')
+            pickle.dump(res, wf)
+            wf.close()
+
+            if verbose:
+                print()
+
+    def delete(self, tickers):
+        if type(tickers) is str:
+            tickers = [tickers]
+
+        for ticker in tickers:
+            file_name = '{}.cache'.format(ticker)
+            file_path = os.path.join(self.cache_path, file_name)
+
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+    def get(self, tickers, acc=False):
+        if type(tickers) is str:
+            tickers = [tickers]
+
+        res = dict()
+        for ticker in tickers:
+            file_name = '{}{}.cache'.format(ticker, '_acc' if acc else '')
+            file_path = os.path.join(self.cache_path, file_name)
+
+            if not os.path.isfile(file_path):
+                continue
+
+            rf = open(file_path, 'rb')
+            res[ticker] = pickle.load(rf)
+            rf.close()
+
+        return res
 
 
-'''
-pd.set_option('display.max_columns', None)
-dm = DataManager()
-tickers = ['^IXIC', '^GSPC', '^DJI',
-           '^IRX', '^FVX', '^TNX', '^TYX',
-           'QQQ', 'QLD', 'TQQQ', 'PSQ', 'QID', 'SQQQ',
-           'BRK-B']
-dm.update(tickers, force_update=False)
-dm.update_acc(tickers, force_update=True, verbose=True)
-'''
+def test():
+    dm = DataManager('./cache')
+
+    tickers = ['^IXIC', 'QQQ', 'QLD', 'TQQQ', 'PSQ', 'QID', 'SQQQ']
+    dm.update(tickers, verbose=True)
+    print()
+    dm.acc_update(tickers, sz_acc_list=[1, 5, 30], verbose=True)
+
+
+if __name__ == '__main__':
+    test()
